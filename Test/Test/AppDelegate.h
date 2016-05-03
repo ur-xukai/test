80fbc7969a0dd27c6d0a58fcdb693cc08d51ae71
weishine <419301422@qq.com>
2016-05-03 15:09:49 +0800

    初始化项目

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by iMac 11 on 16/5/3.
+//  Copyright © 2016年 weishine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by iMac 11 on 16/5/3.
+//  Copyright © 2016年 weishine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
